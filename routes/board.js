67aad37aca49f2e9bfc83085809993d95a704ac4
phantom05 <monster2jy@gmail.com>
2019-10-28 01:23:43 +0900

    게시판 백엔드 구축

--- routes/board.js
+++ routes/board.js
@@ -11,6 +11,7 @@ var {
 var {
   // page
 } = require('./database/query');
+const moment = require('moment');
 
 async function boardListPageQuery(page, callback) {
   return await query(`SELECT COUNT(*) as cnt FROM board`, (result) => {
@@ -24,14 +25,14 @@ async function boardListPageQuery(page, callback) {
     if (totalPageCount < 0) totalPageCount = 0;
 
     var totalPage = Math.ceil(totalPageCount / page_rows_size); // 전체 페이징 수
-    if(totalPage < page_size){
+    if (totalPage < page_size) {
       page_size = totalPage;
     }
-    var totalSet = Math.ceil(totalPage / page_size); 
+    var totalSet = Math.ceil(totalPage / page_size);
     var curSet = Math.ceil(curPage / page_size)
-    var startPage = ((curSet - 1) * page_size) + 1 
+    var startPage = ((curSet - 1) * page_size) + 1
     var endPage = (startPage + page_size) - 1;
-    if(totalPage < endPage){
+    if (totalPage < endPage) {
       endPage = totalPage
     }
     if (curPage < 0) {
@@ -40,7 +41,7 @@ async function boardListPageQuery(page, callback) {
       no = (curPage - 1) * 10
     }
     var result2 = {
-      page:+curPage,
+      page: +curPage,
       page_rows_size,
       page_size,
       totalPage,
@@ -55,6 +56,23 @@ async function boardListPageQuery(page, callback) {
 }
 
 
+// /free/detail/:boardSeq
+router.route('/free/detail')
+  .post(async (req, res, next) => {
+    let body = {};
+    const {
+      boardSeq
+    } = req.body;
+
+    query(`SELECT * FROM board where seq = ${boardSeq}`, (result) => {
+      const data = result[0];
+      body.boardDetail = data;
+      body.result = 1;
+      console.log(body);
+      res.json(body);
+    })
+
+  });
 
 // /board/free/list
 // params : page :1
@@ -69,11 +87,8 @@ router.route('/free/list')
     let offset = (+page - 1) * 10;
     let body = {};
 
-
     boardListPageQuery(page, async (pageInfo) => {
-
-
-      await query(`SELECT * from board limit ${offset},10  `, (result) => {
+      await query(`SELECT * from board order by seq desc limit ${offset},10  `, (result) => {
         console.log(result);
         body.result = 1;
         body.boardList = result;
@@ -82,8 +97,6 @@ router.route('/free/list')
         res.json(body)
       })
     })
-
-
   });
 
 /**
@@ -108,9 +121,9 @@ router.route('/free/upload')
 
   });
 
+
 router.route('/free/delete')
   .post(async (req, res, next) => {
-    console.log('in');
     const {
       boardSeq: seq,
       userSeq
@@ -120,6 +133,27 @@ router.route('/free/delete')
     query(`delete from board where seq='${seq}' and userSeq='${userSeq}' `, (result) => {
       console.log(result);
       body.result = 1;
+      res.json(body)
+    })
+
+  });
+
+
+router.route('/free/modify')
+  .post(async (req, res, next) => {
+    const {boardSeq: seq,userSeq,title,content} = req.body;
+    console.log(seq, userSeq);
+    let body = {};
+    let date = moment().format('YYYY-MM-DD hh:mm:ss');
+    query(`update board SET title='${title}', content='${content}',date='${date}'  where seq='${seq}' and userSeq='${userSeq}'`, (result) => {
+      console.log(result);
+      console.log(result.changedRows,'result.changeRows');
+
+      if(+result.changedRows === 1){
+        body.result = 1;
+      }else{
+        body.result = 2;
+      }
       res.json(body)
     })
 
